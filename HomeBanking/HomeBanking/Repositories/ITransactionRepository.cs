b261c82c95535576a0465893cc8e4904bb765f4f
Matias Bacci <matiasbacci@outlook.es>
2024-05-30 08:42:47 -0300

    Interface transaction created

--- HomeBanking/HomeBanking/Repositories/ITransactionRepository.cs
+++ HomeBanking/HomeBanking/Repositories/ITransactionRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace HomeBanking.Repositories
+﻿using HomeBanking.Models;
+
+namespace HomeBanking.Repositories
 {
     public interface ITransactionRepository
     {
+        IEnumerable<Transaction> GetAllTransactions();
+        void Save(Transaction transaction);
+        Transaction FindById(long id);
     }
 }
